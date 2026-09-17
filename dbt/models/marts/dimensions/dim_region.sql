40122b22ba9de320d794973dfae71e385209a482
Nkanyiso <nknzieec025@student.wethinkcode.co.za>
2026-09-17 15:53:37 +0200

    Add dim_region dbt model

---
+++ dbt/models/marts/dimensions/dim_region.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='table') }}
+
+with years as (
+    select distinct year
+    from {{ ref('stg_health_indicators') }}
+    where year is not null
+)
+
+select
+    (year * 10000 + 101)::int as date_id,
+    make_date(year, 1, 1) as date_day,
+    1 as day,
+    1 as month,
+    year,
+    1 as quarter,
+    'Yearly'::text as reporting_frequency
+from years
